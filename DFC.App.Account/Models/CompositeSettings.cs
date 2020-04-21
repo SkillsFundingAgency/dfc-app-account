20dfcb2b081b1c8789d72754f4df49c13c4742c3
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-04-21 16:24:29 +0100

    wip

---
+++ DFC.App.Account/Models/CompositeSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFC.App.Account.Models
+{
+    public class CompositeSettings
+    {
+        // Path that is registered for this application in the Cosmos 'paths' collection
+        public string Path { get; set; }
+        public string CDN { get; set; }
+    }
+}
