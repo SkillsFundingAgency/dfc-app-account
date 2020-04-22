fd3fbd2afd679a73bda426453370a5597584c644
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-04-22 14:47:47 +0100

    wip

--- DFC.App.Account/ViewModels/ErrorCompositeViewModel.cs
+++ DFC.App.Account/ViewModels/ErrorCompositeViewModel.cs
@@ -8,20 +8,10 @@ namespace DFC.App.Account.ViewModels
 {
     public class ErrorCompositeViewModel : CompositeViewModel
     {
-        private string _rtaCode;
-
-        public string RTACode { get { return _rtaCode; } set { _rtaCode = FormatRTACode(value); } }
-
+       
         public ErrorCompositeViewModel() : base(PageId.Error, "Service Error")
         {
-            _rtaCode = string.Empty;
         }
 
-        private string FormatRTACode(string value)
-        {
-            if (string.IsNullOrWhiteSpace(value)) return string.Empty;
-            var result = Regex.Replace(value.Trim().ToUpper(), ".{4}", "$0 ");
-            return result;
-        }
     }
 }
