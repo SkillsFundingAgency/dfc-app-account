bbf4cbf8c2e5c8c59d4b7e959499bde3bdc11f9c
Liam Wright <mail@liam-wright.co.uk>
2020-04-27 11:16:16 +0100

    Added tests, getting changes from master

---
+++ DFC.App.Account.UnitTests/Controllers/CloseAccountControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using DFC.App.Account.Controllers;
+using DFC.App.Account.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+
+namespace DFC.App.Account.UnitTests.Controllers
+{
+    public class CloseAccountControllerTests
+    {
+        private IOptions<CompositeSettings> _compositeSettings;
+
+
+        [SetUp]
+        public void Init()
+        {
+            _compositeSettings = Options.Create(new CompositeSettings());
+        }
+        [Test]
+        public async Task WhenBodyCalled_ReturnHtml()
+        {
+            var controller = new CloseAccountController(_compositeSettings);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            var result = await controller.Body() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+    }
+}
--- DFC.App.Account.UnitTests/Controllers/HomeControllerTests.cs
+++ DFC.App.Account.UnitTests/Controllers/HomeControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using DFC.App.Account.Controllers;
+﻿using System.Threading.Tasks;
+using DFC.App.Account.Controllers;
 using DFC.App.Account.Models;
 using DFC.App.Account.ViewModels;
 using FluentAssertions;
@@ -8,9 +9,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NSubstitute;
 using NUnit.Framework;
-using System.Threading.Tasks;
 
-namespace DFC.App.Account.UnitTests
+namespace DFC.App.Account.UnitTests.Controllers
 {
     public class HomeControllerTests
     {
@@ -73,6 +73,25 @@ namespace DFC.App.Account.UnitTests
             result.Should().BeOfType<ViewResult>();
             result.ViewName.Should().BeNull();
         }
-        
+        [Test]
+        public void WhenBodyFooterCalled_ReturnHtml()
+        {
+            var controller = new HomeController(_logger, _compositeSettings);
+            var result = controller.BodyFooter() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+        [Test]
+        public void WhenErrorCalled_ReturnHtml()
+        {
+            var controller = new HomeController(_logger, _compositeSettings);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var result = controller.Error() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
     }
 }
