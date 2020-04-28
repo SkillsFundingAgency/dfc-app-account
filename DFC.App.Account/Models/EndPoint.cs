44d33be6c4d7a72f394df8ae6b7efaf8b983ea2e
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-04-28 13:42:19 +0100

    Health Controller

---
+++ DFC.App.Account/Models/EndPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFC.App.Account.Models
+{
+    public class EndPoint
+    {
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public string Methods { get; set; }
+    }
+}
---
+++ DFC.App.Account/ViewModels/EndPointsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DFC.App.Account.Models;
+
+namespace DFC.App.Account.ViewModels
+{
+    public class EndPointsViewModel
+    {
+        public List<EndPoint> EndPoints { get; set; }
+
+        public EndPointsViewModel()
+        {
+            EndPoints = new List<EndPoint>();
+        }
+    }
+}
