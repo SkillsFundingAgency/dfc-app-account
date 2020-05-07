025a551894ee303eb3394bd42c2d143ea641ea4f
Liam Wright <mail@liam-wright.co.uk>
2020-05-07 15:09:49 +0100

    Edited as per comments

--- DFC.App.Account.Services.SHC/Models/SkillsDocument.cs
+++ DFC.App.Account.Services.SHC/Models/SkillsDocument.cs
@@ -2,35 +2,35 @@
 
 namespace DFC.App.Account.Services.SHC.Models
 {
-	[XmlRoot(ElementName = "SkillsDocument", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+	[XmlRoot(ElementName = "SkillsDocument", Namespace = XmlNamespaces.EntitiesNamespace)]
 	public class SkillsDocument
     {
 
-		[XmlElement(ElementName = "DocumentId", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "DocumentId", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string DocumentId { get; set; }
-		[XmlElement(ElementName = "SkillsDocumentType", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "SkillsDocumentType", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string SkillsDocumentType { get; set; }
-		[XmlElement(ElementName = "CreatedAt", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "CreatedAt", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string CreatedAt { get; set; }
-		[XmlElement(ElementName = "CreatedBy", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "CreatedBy", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string CreatedBy { get; set; }
-		[XmlElement(ElementName = "UpdatedAt", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "UpdatedAt", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public UpdatedAt UpdatedAt { get; set; }
-		[XmlElement(ElementName = "UpdatedBy", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "UpdatedBy", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string UpdatedBy { get; set; }
-		[XmlElement(ElementName = "DeletedAt", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "DeletedAt", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public DeletedAt DeletedAt { get; set; }
-		[XmlElement(ElementName = "DeletedBy", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "DeletedBy", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string DeletedBy { get; set; }
-		[XmlElement(ElementName = "ExpiresTimespan", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "ExpiresTimespan", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string ExpiresTimespan { get; set; }
-		[XmlElement(ElementName = "ExpiresType", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "ExpiresType", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string ExpiresType { get; set; }
-		[XmlElement(ElementName = "DataValues", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "DataValues", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public DataValues DataValues { get; set; }
-		[XmlElement(ElementName = "Identifiers", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "Identifiers", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public Identifiers Identifiers { get; set; }
-		[XmlElement(ElementName = "SkillsDocumentTitle", Namespace = "http://schemas.skillsfundingagency.bis.gov.uk/alse/skillscentral/1.0/Entities")]
+		[XmlElement(ElementName = "SkillsDocumentTitle", Namespace = XmlNamespaces.EntitiesNamespace)]
 		public string SkillsDocumentTitle { get; set; }
 	}
 }
