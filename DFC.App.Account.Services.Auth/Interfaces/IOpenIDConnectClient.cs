0f1d6db9e1fc32700a21bef8110187613c847e3e
Matt Daniels <matt@techtoniq.com>
2020-05-13 14:15:28 +0100

    Azure B2C OIDC Service

---
+++ DFC.App.Account.Services.Auth/Interfaces/IOpenIDConnectClient.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+namespace DFC.App.Account.Services.AzureB2CAuth.Interfaces
+{
+    public interface IOpenIDConnectClient
+    {
+        Task<string> GetRegisterUrl();
+        Task<string> GetSignInUrl();
+        Task<JwtSecurityToken> ValidateToken(string token);        
+    }
+}
