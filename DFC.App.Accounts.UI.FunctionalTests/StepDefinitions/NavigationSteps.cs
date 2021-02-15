4032194ec82e122f28f5466560e87fdc9d810b62
kadir_malik <kadir_malik@hotmail.com>
2021-02-15 09:30:47 +0000

    Create UI Automation project for Accounts application

---
+++ DFC.App.Accounts.UI.FunctionalTests/StepDefinitions/NavigationSteps.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="NavigationSteps.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.App.Accounts.Model;
+using DFC.App.Accounts.UI.FunctionalTests.Pages;
+using DFC.TestAutomation.UI.Extension;
+using OpenQA.Selenium;
+using System;
+using System.Globalization;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.Accounts.UI.FunctionalTests.StepDefinitions
+{
+    [Binding]
+    internal class NavigationSteps
+    {
+        public NavigationSteps(ScenarioContext context)
+        {
+            this.Context = context;
+        }
+
+        private ScenarioContext Context { get; set; }
+
+        [Given(@"I am on the (.*) page")]
+        public void GivenIAmOnThePage(string pageName)
+        {
+            switch (pageName.ToLower(CultureInfo.CurrentCulture))
+            {
+                case "action plans landing":
+                    var actionplansHomePage = new ActionPlansLandingPage(this.Context);
+                    actionplansHomePage.NavigateToActionPlansPage();
+                    var pageHeadingLocator = By.ClassName("govuk-heading-xl");
+                    this.Context.GetHelperLibrary<AppSettings>().WebDriverWaitHelper.WaitForElementToContainText(pageHeadingLocator, "Action plans");
+                    break;
+
+                default:
+                    throw new OperationCanceledException($"Unable to perform the step: {this.Context.StepContext.StepInfo.Text}. The page name provided was not recognised.");
+            }
+        }
+    }
+}
---
+++ DFC.App.Accounts.UI.FunctionalTests/StepDefinitions/ValidationSteps.cs
@@ -0,0 +1,86 @@
+﻿// <copyright file="ValidationSteps.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.App.Accounts.Model;
+using DFC.TestAutomation.UI;
+using DFC.TestAutomation.UI.Extension;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.Accounts.UI.FunctionalTests.StepDefinitions
+{
+    [Binding]
+    internal class ValidationSteps
+    {
+        public ValidationSteps(ScenarioContext context)
+        {
+            this.Context = context;
+        }
+
+        private ScenarioContext Context { get; set; }
+
+        [Then(@"I am taken to the (.*) page")]
+        public void ThenIAmTakenToThePage(string pageName)
+        {
+            By locator = null;
+
+            switch (pageName.ToLower(CultureInfo.CurrentCulture))
+            {
+                case "sign in":
+                    locator = By.CssSelector("h1");
+                    break;
+
+                case "your account":
+                    locator = By.ClassName("govuk-heading-xl");
+                    break;
+
+                case "your details":
+                    locator = By.ClassName("govuk-heading-xl");
+                    break;
+
+                case "edit your details":
+                    locator = By.ClassName("govuk-heading-xl");
+                    break;
+
+                default:
+                    locator = By.CssSelector("h1.govuk-fieldset__heading");
+                    break;
+            }
+
+            this.Context.GetHelperLibrary<AppSettings>().WebDriverWaitHelper.WaitForElementToContainText(locator, pageName);
+        }
+
+        [Then(@"the (.*) field contains the updated details")]
+        public void ThenTheHomeNumberFieldContainsTheUpdatedNumber(string fieldLabel)
+        {
+            bool updateSuccesful = false;
+
+            IWebElement tableElement = this.Context.GetWebDriver().FindElement(By.XPath("//*[@id='main-content']/div/div/div/div/div/div/div[1]/div/form/div[1]/table[2]"));
+
+            IList<IWebElement> trCollection = tableElement.FindElements(By.TagName("tr"));
+
+            IList<IWebElement> tdCollection;
+
+            foreach (IWebElement element in trCollection)
+            {
+                tdCollection = element.FindElements(By.TagName("td"));
+                string column1 = tdCollection[0].Text;
+                string column2 = tdCollection[1].Text;
+                if (column2.Equals(this.Context.Get<IObjectContext>().GetObject("HomeNumber")))
+                {
+                    updateSuccesful = true;
+                }
+            }
+
+            if (!updateSuccesful)
+            {
+                throw new NotFoundException($"Unable to perform the step: {this.Context.StepContext.StepInfo.Text}. Phone number update unsuccessful");
+            }
+        }
+    }
+}
