d49b03b44531de58f1081493721f47fdd1de5724
Stephen Yates <SYatesSoftware@gmail.com>
2021-07-07 11:23:07 +0100

    unit test updates

---
+++ DFC.App.Account.Application.UnitTests/HtmlExtensionTests.cs
@@ -0,0 +1,50 @@
+﻿using DFC.App.Account.Application.Common.Extensions;
+using NUnit.Framework;
+
+namespace DFC.App.Account.Application.UnitTests
+{
+    public class HtmlExtensionTests
+    {
+
+        private const string DobField = "dob";
+        private const string DobDay = "day";
+        private const string DobMonth = "month";
+        private const string DobYear = "year";
+        private const string ErrorClass = "govuk-form-group--error";
+
+        [Test]
+        public void When_GetClassCalledForErrorState_ReturnCorrectClass()
+        {
+            var model = new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary();
+            model.AddModelError("test", "error");
+            var result  = HtmlExtensions.GetClass("test", model);
+
+            Assert.AreEqual(ErrorClass, result);
+        }
+
+        [Test]
+        public void When_GetClassCalledForValidState_ReturnCorrectClass()
+        {
+            var model = new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary();
+            var result = HtmlExtensions.GetClass("test", model);
+
+            Assert.IsEmpty(result);
+        }
+        
+        [TestCase(DobField, ErrorClass)]
+        [TestCase(DobDay, ErrorClass)]
+        [TestCase(DobMonth, ErrorClass)]
+        [TestCase(DobYear, ErrorClass)]
+        [TestCase("test", "")]
+        public void When_DobVariableGetClassCalled_ReturnCorrectClass(string field, string expected)
+        {
+           
+
+            var model = new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary();
+            model.AddModelError(field, "error");
+            var result = HtmlExtensions.GetDOBClass(DobField, DobDay, DobMonth, DobYear, model);
+
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
---
+++ DFC.App.Account.Application.UnitTests/ServiceFunctionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DFC.App.Account.Application.Common.Services;
+using NUnit.Framework;
+
+namespace DFC.App.Account.Application.UnitTests
+{
+    public class ServiceFunctionTests
+    {
+
+        [Test]
+        public void When_IsRegexCalledWithEmptyString_ReturnFalse()
+        {
+            var result = ServiceFunctions.IsValidRegexValue("", "");
+            Assert.False(result);
+        }
+
+
+        [Test]
+        public void When_IsRegexCalledWithValidString_ReturnTrue()
+        {
+            var result = ServiceFunctions.IsValidRegexValue("07867565456", "^(\\+44\\s?7\\d{3}|\\(?07\\d{3}\\)?)\\s?\\d{3}\\s?\\d{3}$");
+            Assert.True(result);
+        }
+    }
+}
