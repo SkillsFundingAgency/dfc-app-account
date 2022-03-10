007670d1321ebbae12adeba37296eeaf3493d693
LAPTOP-CTLLJBUM\man_l <man_luck@yahoo.com>
2022-03-10 09:52:28 +0000

    FT2-1322

---
+++ DFC.App.Account.UnitTests/Controllers/SitemapTests.cs
@@ -0,0 +1,25 @@
+﻿using DFC.App.Account.Controllers;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace DFC.App.Account.UnitTests.Controllers
+{
+    public class SitemapTests
+    {
+        private readonly SitemapController controller;
+
+        public SitemapTests()
+        {
+            controller = new SitemapController(A.Fake<ILogger<SitemapController>>());
+        }
+
+        [Fact]
+        public void SitemapGeneratedSuccesfully()
+        {
+            var actionResponse = controller.SitemapView();
+            Assert.IsType<ContentResult>(actionResponse);
+        }
+    }
+}
