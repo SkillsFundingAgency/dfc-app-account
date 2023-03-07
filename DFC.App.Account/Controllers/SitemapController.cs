86e7abb1b93de2984ecb63d08fac86bfcaf1f09f
Joe Hudson-Small <Joehs@me.com>
2023-03-07 15:11:42 +0000

    Adds NCSLT-2612 to Release 27.1 (#200)
    * FT2-1322 (#189)
    
    Co-authored-by: LAPTOP-CTLLJBUM\man_l <man_luck@yahoo.com>
    
    * Updating nuget package
    
    * Reverted change seemiingly incorrectly commited (#193)
    
    * dotne6 upgrade
    
    * remove dotnetcore reference in azure-pipelines.yml
    
    * changed the dss-contactdetails api version to 1 in arm template
    
    * personalisation packages and newtonsoft.json upgrade
    
    * DFC.Content.Pkg.Netcore dotnet6 upgrade
    
    ---------
    
    Co-authored-by: manluck1 <93193560+manluck1@users.noreply.github.com>
    Co-authored-by: LAPTOP-CTLLJBUM\man_l <man_luck@yahoo.com>
    Co-authored-by: Andy Rose <andy@cyberian.co.uk>
    Co-authored-by: ryanbarlowmethods <85944633+ryanbarlowmethods@users.noreply.github.com>
    Co-authored-by: LarryOG <43647901+LarryOG@users.noreply.github.com>

---
+++ DFC.App.Account/Controllers/SitemapController.cs
@@ -0,0 +1,63 @@
+﻿using DFC.App.Account.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DFC.App.Account.Models;
+using DFC.Compui.Cosmos.Enums;
+using System.Net.Mime;
+
+namespace DFC.App.Account.Controllers
+{
+    public class SitemapController : Controller
+    {
+
+        private readonly ILogger<SitemapController> logger;
+
+        public SitemapController(ILogger<SitemapController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        [Route("pages/sitemap")]
+        public IActionResult SitemapView()
+        {
+            return Sitemap();
+        }
+
+        [HttpGet]
+        [Route("/sitemap.xml")]
+        public IActionResult Sitemap()
+        {
+            try
+            {
+                logger.LogInformation("Generating Sitemap");
+
+                var sitemapUrlPrefix = $"{Request.GetBaseAddress()}".TrimEnd('/');
+                var sitemap = new Sitemap();
+                sitemap.Add(new SitemapLocation
+                {
+                    Url = $"{sitemapUrlPrefix}/your-account",
+                    Priority = 0.5,
+                    ChangeFrequency = SiteMapChangeFrequency.Weekly,
+                });
+
+                var xmlString = sitemap.WriteSitemapToString();
+                logger.LogInformation("Generated Sitemap");
+
+                return Content(xmlString, MediaTypeNames.Application.Xml);
+            }
+#pragma warning disable CA1031
+            catch (System.Exception ex)
+#pragma warning restore CA1031
+            {
+                logger.LogError(ex, "{NameofSitemap}: {ExMessage}", nameof(Sitemap), ex.Message);
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ DFC.App.Account/Models/Sitemap.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DFC.App.Account.Models
+{
+    [XmlRoot("urlset", Namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")]
+    [ExcludeFromCodeCoverage]
+    public class Sitemap
+    {
+        private readonly ArrayList map;
+
+        public Sitemap()
+        {
+            map = new ArrayList();
+        }
+
+        [XmlIgnore]
+        public IEnumerable<SitemapLocation>? Mappings => map as IEnumerable<SitemapLocation>;
+
+        [XmlElement("url")]
+        public SitemapLocation[] Locations
+        {
+            get
+            {
+                SitemapLocation[] items = new SitemapLocation[map.Count];
+                map.CopyTo(items);
+
+                return items;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    return;
+                }
+
+                SitemapLocation[] items = value;
+                map.Clear();
+
+                foreach (SitemapLocation item in items)
+                {
+                    map.Add(item);
+                }
+            }
+        }
+
+        public int Add(SitemapLocation item)
+        {
+            return map.Add(item);
+        }
+
+        public void AddRange(IEnumerable<SitemapLocation> locs)
+        {
+            if (locs == null)
+            {
+                throw new ArgumentNullException(nameof(locs));
+            }
+
+            foreach (var i in locs)
+            {
+                map.Add(i);
+            }
+        }
+
+        public string WriteSitemapToString()
+        {
+            using var sw = new StringWriter();
+            var ns = new XmlSerializerNamespaces();
+
+            var xs = new XmlSerializer(typeof(Sitemap));
+            xs.Serialize(sw, this, ns);
+            return sw.GetStringBuilder().ToString();
+        }
+    }
+}
